8ed7eb670ca1ea6bab1902d19b24b2652359ae80
Aylla Beatriz <ayllabeatriz442@gmail.com>
2022-07-05 19:29:46 -0300

    Formatando o formulário para criar transações e salvando as informações

--- public/js/home.js
+++ public/js/home.js
@@ -6,6 +6,27 @@ let data = {
     transactions: []
 };
 
+document.getElementById("button-logout").addEventListener("click", logout);
+
+// Adicionar Lançamento
+document.getElementById("transaction-form").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const value = parseFloat(document.getElementById("value-input").value);
+    const description = document.getElementById("description-input").value;
+    const date = document.getElementById("date-input").value;
+    const type = document.querySelector('input[name="type-input"]:checked').value;
+
+    data.transactions.unshift({
+        value: value, type: type, description: description, date: date
+    });
+
+    saveData(data);
+    myModal.hide();
+    e.target.reset();
+    alert("Lançamento adicionado com sucesso!");
+})
+
 // Chamando a função que vai verificar se há um usuário logado ou não
 checkLogged();
 
@@ -28,4 +49,16 @@ function checkLogged() {
     if (dataUser) {
         data = JSON.parse(dataUser);
     }
+}
+
+// Função responsável por deslogar o usuário
+function logout () {
+    sessionStorage.removeItem("logged");
+    localStorage.removeItem("session");
+
+    window.location.href = "index.html";
+}
+
+function saveData(data) {
+    localStorage.setItem(data.login, JSON.stringify(data));
 }
